af0a07f6e844b40d8a7e54d88962f9a69bfe9a1a
gathumartins <gathumartins@gmail.com>
2024-04-12 11:12:07 +0300

    fixing the coming soon page responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,16 +19,16 @@ export default function Home() {
         <Link href="tel:+254 728 429 252" className="flex group flex-row gap-2 place-items-center text-[#EF801E] hover:text-[#EF801E]"><MdOutlineLocalPhone className="h-[18px] w-[18px]"/>
 <span className="text-[#0A475C] group-hover:text-[#EF801E] text-base hidden sm:inline-block font-normal">+254 728 429 252</span></Link>
       </header>
-      <main className="my-[80px]">
-        <section className="flex flex-col sm:flex-row justify-between">
+      <main className="my-[50px] sm:my-[80px]">
+        <section className="flex flex-col sm:flex-row justify-between gap-7">
           <div className="text-center sm:text-left flex justify-start flex-col sm:justify-center">
           <div><Image src="/imgs/logo2.svg" alt="Creative Geeks logo" width={89} height={70} className="inline-block"/></div>
           <h2 className="text-[#EF801E] font-normal text-4xl mt-[30px]">We’re</h2>
           <h1 className="text-[#0A475C] font-black text-4xl">Launching Soon!</h1>
-          <p className="text-black max-w-[420px] text-sm font-normal mt-[20px]">Our new website is coming soon, with a user-friendly experience. Stay tuned for the big reveal!</p>
+          <p className="text-black max-w-[420px] mx-auto sm:mx-0 text-sm font-normal mt-[20px]">Our new website is coming soon, with a user-friendly experience. Stay tuned for the big reveal!</p>
           </div>
         <div className="flex justify-center sm:justify-end">
-          <Image src="/imgs/coming.svg" alt="creative geeks coming soon iamge" width={657} height={416} className="inline-block" />
+          <Image src="/imgs/coming.svg" alt="creative geeks coming soon iamge" width={657} height={416} className="inline-block w-[70%] sm:w-[100%]" />
         </div>
         </section>
       </main>
